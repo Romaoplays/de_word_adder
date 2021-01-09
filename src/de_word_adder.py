f9cbda950c755d5259275559d737b04ccafe319e
Gabriel Romao <romaoplays@hotmail.com>
2021-01-08 21:50:49 -0300

    Fixed IndexError when word had less than 5 translations, added message when no translations were found. Fixed csv encoding issue.

--- src/de_word_adder.py
+++ src/de_word_adder.py
@@ -6,9 +6,9 @@ from pons_translator_module import get_translation
 
 # Creates csv file if it doesn't exist
 try:
-    csv_file = open("de_words.csv")
+    csv_file = open("de_words.csv", encoding="utf-8-sig")
 except FileNotFoundError:
-    csv_file = open("de_words.csv", "w")
+    csv_file = open("de_words.csv", "w", encoding="utf-8-sig")
 finally:
     csv_file.close()
 
@@ -17,9 +17,17 @@ def get_word_input():
     print("\nType out word:")
     resposta = input()
     translated_words = get_translation(resposta)
+    if len(translated_words[0]) == 1:
+        print("\n!!!Translation not found!!!")
+        return None
+    elif len(translated_words[0]) < 6:
+        f = len(translated_words[0]) - 1
+    else:
+        f = 5
+
     print("\nPossible translations: ")
     unduper_list = []
-    for i in range(5):
+    for i in range(f):
         if translated_words[1][i + 1] not in unduper_list:
             print("- " + translated_words[1][i + 1])
             unduper_list.append(translated_words[1][i + 1])
@@ -38,22 +46,23 @@ while True:
 
     if resposta_inicial == "1" or resposta_inicial == "2":
         words = get_word_input()
-        print("\n1 - Add \n2 - Discard")
-        resposta_add = input()
-        words_list = list(words)
+        if words is not None:
+            print("\n1 - Add \n2 - Discard")
+            resposta_add = input()
+            words_list = list(words)
 
-        if resposta_inicial == "2" and resposta_add == "1":
-            print("\nBook page?")
-            words_list.append("Pag. " + input())
+            if resposta_inicial == "2" and resposta_add == "1":
+                print("\nBook page?")
+                words_list.append("Pag. " + input())
 
-        if resposta_add == "1":
-            csv_file = open("de_words.csv", "a")
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(words_list)
-            csv_file.close()
+            if resposta_add == "1":
+                csv_file = open("de_words.csv", "a", encoding="utf-8-sig")
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow(words_list)
+                csv_file.close()
 
     elif resposta_inicial == "3":
-        csv_file = open("de_words.csv", "r")
+        csv_file = open("de_words.csv", "r", encoding="utf-8-sig")
         csv_reader = csv.reader(csv_file)
         csv_list = list(csv_reader)
 
@@ -81,7 +90,7 @@ while True:
                 csv_file.close()
                 os.remove("de_words.csv")
                 print("\nThe file has been cleared!")
-                csv_file = open("de_words.csv", "w")
+                csv_file = open("de_words.csv", "w", encoding="utf-8-sig")
                 csv_file.close()
                 break
             elif resposta_viewer == "ok":
@@ -91,3 +100,6 @@ while True:
                     else:
                         print(csv_list[i])
                 k = k + 1
+
+    else:
+        print("!!!Please type one of the numbers above!!!")
